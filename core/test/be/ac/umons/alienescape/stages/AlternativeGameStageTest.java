8c4ec115d8b9e55416bf5d7393f8e78e83709c3d
Adrien Coppens <qdrien@gmail.com>
2019-09-23 16:50:57 +0200

    Add quick example to show how to use the @Mock annotation

---
+++ core/test/be/ac/umons/alienescape/stages/AlternativeGameStageTest.java
@@ -0,0 +1,126 @@
+package be.ac.umons.alienescape.stages;
+
+import be.ac.umons.alienescape.actors.Runner;
+import be.ac.umons.alienescape.box2d.EnemyUserData;
+import be.ac.umons.alienescape.box2d.RunnerUserData;
+import be.ac.umons.alienescape.box2d.UserData;
+import be.ac.umons.alienescape.utils.WorldUtils;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.World;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.when;
+
+/**
+ * Shows how to use the @Mock annotation to inject a mock (more readable, easier to debug).
+ * The whole class is equivalent to the {@link GameStageTest} class.
+ */
+public class AlternativeGameStageTest {
+
+    private static HeadlessApplication application;
+    private World world;
+
+    /*
+    The following line (using the @Mock annotation) can replace the combination of:
+    private Contact contact;
+    and a manual mock instantiation:
+    contact = Mockito.mock(Contact.class);
+
+    But requires a call (e.g. in a SetUp method) to:
+    MockitoAnnotations.initMocks(this);
+    */
+    @Mock private Contact contact;
+
+    @Mock private SpriteBatch batch;
+    @Mock private Animation animationMock;
+    @Mock private TextureRegion textureMock;
+
+    @BeforeClass
+    public static void init() {
+        application = new HeadlessApplication(new ApplicationListener() {
+            @Override
+            public void create() {
+            }
+
+            @Override
+            public void resize(int width, int height) {
+            }
+
+            @Override
+            public void render() {
+            }
+
+            @Override
+            public void pause() {
+            }
+
+            @Override
+            public void resume() {
+            }
+
+            @Override
+            public void dispose() {
+            }
+        });
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        world = new World(Vector2.Zero, true);
+    }
+
+    @Test
+    public void RunnerVsEnemyHitTest() {
+
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+
+        Runner runner = new Runner(WorldUtils.createRunner(world),
+                animationMock, textureMock, textureMock, textureMock);
+        GameStage gameStage = new GameStage(batch, runner);
+
+        Body bodyA = Mockito.mock(Body.class);
+        Fixture fixtureA = Mockito.mock(Fixture.class);
+        UserData userDataA = new RunnerUserData(1f, 1f);
+        when(bodyA.getUserData()).thenReturn(userDataA);
+        when(fixtureA.getBody()).thenReturn(bodyA);
+        when(contact.getFixtureA()).thenReturn(fixtureA);
+
+        Body bodyB = Mockito.mock(Body.class);
+        Fixture fixtureB = Mockito.mock(Fixture.class);
+        UserData userDataB = new EnemyUserData(1f, 1f, null);
+        when(bodyB.getUserData()).thenReturn(userDataB);
+        when(fixtureB.getBody()).thenReturn(bodyB);
+        when(contact.getFixtureB()).thenReturn(fixtureB);
+
+        assertFalse(runner.isHit());
+        gameStage.beginContact(contact);
+        assertTrue(runner.isHit());
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        application.exit();
+    }
+}
